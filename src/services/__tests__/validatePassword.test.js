1117e999ee75783ac6f1d2e1bc8b08cd43c490f4
Iuliana0123 <113596664+Iuliana0123@users.noreply.github.com>
2022-12-21 12:25:24 +0100

    test3

--- src/services/__tests__/validatePassword.test.js
+++ src/services/__tests__/validatePassword.test.js
@@ -9,4 +9,8 @@ describe('validatePassword test suite', () => {
     it('should return true given a password 8 char or longer, a letter and a number', () => {
         expect(validatePassword("0123456test")).toBe(true);
     });
+
+    it('should return false given a password 8 char or longer, without letters', () => {
+        expect(validatePassword("0123456")).toBe(false);
+    });
 });
