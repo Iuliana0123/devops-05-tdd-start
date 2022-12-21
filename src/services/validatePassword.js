0c9c2b31e8c7836dce7213da06826eb3afc98600
Iuliana0123 <113596664+Iuliana0123@users.noreply.github.com>
2022-12-21 12:27:40 +0100

    refactor

--- src/services/validatePassword.js
+++ src/services/validatePassword.js
@@ -1,8 +1,7 @@
 /*eslint linebreak-style: ["error", "windows"]*/
 module.exports = function (password) {
-    if (password.length >= 8){
-        return true;
-    }
-    return false;
+    const validLength = password.length >= 8;
+    const containsLetter = /[a-z]/g.test(password);
+    return validLength && containsLetter;
 };
 
